0f03b102d9907465e19b77ca7089549b697969d2
lewis-kori <lewiskosh@gmail.com>
2021-01-04 09:45:28 +0300

    event attendance functionality added

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import HomePageTemplateView, UserProfilePage
+from .views import (BookEventAttendanceAPIView, EventDetailPage,
+                    HomePageTemplateView, UserProfilePage)
 
 app_name = 'home'
 
 urlpatterns = [
     path('',HomePageTemplateView.as_view(), name='homepage'),
     path('profile/',UserProfilePage.as_view(), name='my_profile'),
+    path('events/<int:pk>/',EventDetailPage.as_view(), name='event'),
+    path('events/booking/',BookEventAttendanceAPIView.as_view(), name='event_booking'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,12 @@
-from core.models import Appointment
+from core.models import Appointment, Event
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.shortcuts import get_object_or_404
+from django.views.generic import DetailView, TemplateView
+from rest_framework.views import APIView
 from users.models import Department
+from rest_framework.response import Response
+from core.mixins import CSRFExemptMixin
 
 User = get_user_model()
 
@@ -42,3 +46,32 @@ class UserProfilePage(LoginRequiredMixin, TemplateView):
             patient=user, status='declined')
         context["events_attending"] = user.events_attending.all().order_by('-pk')
         return context
+
+
+class EventDetailPage(DetailView):
+    template_name = 'main/events/details.html'
+    model = Event
+    context_object_name = 'event'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['events'] = Event.objects.all().exclude(id=self.object.id)[0:6]
+        return context
+
+
+class BookEventAttendanceAPIView(APIView):
+    authentication_classes = (CSRFExemptMixin, )
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        data = request.data
+
+        action = data['action']
+        event = get_object_or_404(Event, id=data['event'])
+
+        if action == 'attending':
+            event.attendees.add(user)
+        else:
+            event.attendees.remove(user)
+        event.save()
+
+        return Response({'detail': 'Event succesfully update'})
